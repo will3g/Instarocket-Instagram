ad2031c9d9c392814734e1b3fea0155071a60177
WillDafCom2 <willianpaulo_09@hotmail.com>
2019-08-16 23:26:54 -0300

    primeiro commit da parte mobile

---
+++ instarocket/src/pages/Feed.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Image, TouchableOpacity } from 'react-native';
+import camera from '../img/assets/camera.png';
+
+export default class Feed extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerRight: (
+      <TouchableOpacity style={{ marginRight: 20 }} onPress={() => navigation.navigate('New')}>
+        <Image source={camera}/>
+      </TouchableOpacity>
+    ),
+  });
+
+  render() {
+    return <View />;
+  }
+}
---
+++ instarocket/src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createAppContainer, createStackNavigator, HeaderBackButton } from 'react-navigation';
+import { Image } from 'react-native';
+
+import Feed from './pages/Feed';
+import New from './pages/New';
+
+import logo from './img/assets/logo.png';
+
+export default createAppContainer(
+  createStackNavigator({
+    Feed,
+    New,
+  }, {
+    defaultNavigationOptions: {
+      headerTintColor: '#000',
+      headerTitle: <Image source={logo} style={{ marginHorizontal: 20 }}/>,
+      headerBackTitle: null,
+    },
+  })
+);
